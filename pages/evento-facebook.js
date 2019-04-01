cab6f44614dc0a66742d7e7a0d008559484467fe
Ruggeri Oliveira Souza <rolivsou@everis.com>
2019-04-01 18:19:20 -0300

    alterado metodo para clicar no link eventos

--- pages/evento-facebook.js
+++ pages/evento-facebook.js
@@ -1,12 +1,21 @@
-describe('angularjs homepage todo list', function() {
+describe('Enviar Mensagem de Aniversário', function() {
 
-    var linkEventos = element(by.xpath("//div[text()='Eventos']"));
+    var linkEventos = element(by.css("a[data-testid='left_nav_item_Eventos']"));
     var linkAniversario = element(by.xpath("//span[text()='Aniversários']/.."));
+    var labelEventos = element(by.xpath("(//span[text()='Eventos'])[1]"));
+    var divBlock = element(by.xpath("//div[@class='_3ixn']"));
+
+    it('clicar em eventos', function() {
+        browser.refresh();
+        divBlock.click();
+        linkEventos.click();
+        browser.driver.sleep(3000);
+        expect(labelEventos.getText()).toEqual("Eventos");
+    }); 
 
     it('Mandar mensagem', function() {
-        linkEventos.click();
+        browser.driver.sleep(999000);
         linkAniversario.click();
-
-    }); 
+    });
 
 });
--- pages/login-facebook.js
+++ pages/login-facebook.js
@@ -2,11 +2,15 @@ describe('Logar e Validar login no Facebook', function() {
     
     var email = element(by.id('email'));
     var senha = element(by.id('pass'));
-    var paginaInicial = element(by.xpath("//div[@dataclick='home_icon']"));
+    var paginaInicial = element(by.xpath("//div[@data-click='home_icon']"));
     var goButton = element(by.xpath("//label[@id='loginbutton']/input"));
 
     beforeEach(function() {
         browser.get('https://www.facebook.com/');
+    });
+
+    it('Verificar Titulo', function() {
+        expect(browser.getTitle()).toEqual('Facebook – entre ou cadastre-se');
     });
 
     it('Logar no Facebook', function() {
@@ -20,8 +24,4 @@ describe('Logar e Validar login no Facebook', function() {
         expect(paginaInicial.getText()).toEqual("Página inicial");
     });
 
-    it('Verificar Titulo', function() {
-        expect(browser.getTitle()).toEqual('Facebook – entre ou cadastre-se');
-    });
-    
 });
